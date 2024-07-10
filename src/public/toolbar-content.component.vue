8d84bfe1b7aa6919f596d5d9630375fa547570a1
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-07-10 09:09:22 -0500

    feat/style: added navbar navigation features and defined the custom properties

--- src/public/toolbar-content.component.vue
+++ src/public/toolbar-content.component.vue
@@ -5,10 +5,15 @@ export default {
 </script>
 
 <template>
-   <div class="toolbar-manager bg-cyan-800 p-5">
+   <div class="toolbar-manager flex justify-content-between bg-cyan-800 p-5">
       <div class="text-white">
          <h3 class="text-xs sm:text-base md:text-lg"> Inspection Center </h3>
       </div>
+        <ul class="flex gap-3 text-white text-xl">
+          <li> Home</li>
+          <li> Sign in </li>
+          <li> My Profile </li>
+        </ul>
    </div>
 </template>
 
