329cd91de4c91e8b0c2f7af2b76d896e3578a464
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-07-26 07:38:13 -0500

    style: added linear-gradient for background images

--- src/locale/sign-up-language/sign_up_en.js
+++ src/locale/sign-up-language/sign_up_en.js
@@ -2,13 +2,13 @@ const en = {
     title_sign_up: 'Please give your account details here:',
     sign_up_section: 'Sign up',
     sign_in : 'Sign in',
-    ruc: 'Please enter your dni or ruc',
+    ruc: 'Please enter your ruc',
     username: 'Please enter your username',
     email: 'Please enter your email',
     social_reason: 'Please enter your name or social reason',
     password: 'Please enter your password',
     warning_sign_up: 'Do you already have an account?',
-    ruc_warning: 'Remember your ruc or dni must be contain only numbers and have 11',
+    ruc_warning: 'Remember your ruc  must be contain only numbers and have 11',
     email_warning: 'Remember you need to send a valid email',
     password_warning: 'Your password must have at least 8 characters between letters, numbers and at least one special character'
 }
--- src/locale/sign-up-language/sign_up_es.js
+++ src/locale/sign-up-language/sign_up_es.js
@@ -1,13 +1,13 @@
 const es = {
   title_sign_up: 'Por favor, ingrese sus datos de cuenta aquí:',
   sign_in : 'Iniciar sesión',
-  ruc: 'Por favor, ingrese su dni o ruc',
+  ruc: 'Por favor, ingrese su ruc',
   username: 'Por favor, ingrese su nombre de usuario',
   email: 'Por favor, ingrese su correo electrónico',
   social_reason: 'Por favor, ingrese su nombre o razón social',
   password: 'Por favor, ingrese su contraseña',
   warning_sign_up: '¿Ya tiene una cuenta registrada?',
-  ruc_warning: 'Recuerde que su ruc o dni debe contener solo números y tener 11 dígitos',
+  ruc_warning: 'Recuerde que su ruc debe contener solo números y tener 11 dígitos',
   email_warning: 'Recuerde que necesita enviar un correo electrónico válido',
   password_warning: 'Su contraseña debe tener como minimo  de 8 cáracteres entre letras, números y como minimo un caracter especial',
 }
