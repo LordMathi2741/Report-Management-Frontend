4b882d4ab31e04fe5de710e1c1153fab67a67080
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-08-07 18:36:35 -0500

    fix: fixed idented error with divs

--- src/components/search-report-options.component.vue
+++ src/components/search-report-options.component.vue
@@ -5,17 +5,16 @@ import { isTokenExpired } from '@/helpers/verify-token.service.js'
 
 export default {
   name: 'search-report-options',
-  data(){
+  data() {
     return {
-      reportImg : "",
+      reportImg: "",
       certifiedNumber: "",
       cylinderNumber: "",
-      vehicleIdentifier:"",
+      vehicleIdentifier: "",
       emitDate: null,
-      reportsService : new ReportsService(),
+      reportsService: new ReportsService(),
       notDataFound: true,
       reportsImgService: new ReportImgService()
-
     }
   },
   methods: {
@@ -24,25 +23,24 @@ export default {
         .then((response) => {
           if (response.status === 200) {
             this.notDataFound = false;
-
-            this.getReportImg(" " + this.certifiedNumber);
+            this.getReportImg(this.certifiedNumber);
           }
         }).catch(() => {
         this.notDataFound = true;
         alert("No report found, please check the parameters");
       })
     },
-    async getReportImg(filename){
+    async getReportImg(filename) {
       await this.reportsImgService.getReportImgByFileName(filename)
         .then((response) => {
           if (response.status === 200) {
             this.reportImg = response.data;
           }
         }).catch(() => {
-        this.notDataFound = true;
-      })
+          this.notDataFound = true;
+        })
     },
-    clearParameters(){
+    clearParameters() {
       this.certifiedNumber = "";
       this.cylinderNumber = "";
       this.vehicleIdentifier = "";
@@ -52,7 +50,7 @@ export default {
   },
   computed: {
     pdfImgUrl() {
-      return `http://localhost:3000/pdfs/${this.certifiedNumber}.pdf#toolbar=0`;
+      return `http://38.25.38.94:3000/pdfs/${this.certifiedNumber}.pdf#toolbar=0`;
     },
     cylinderNumberPlaceholder() {
       return this.$t('cylinder_number_placeholder');
@@ -67,7 +65,7 @@ export default {
       return this.$t('certificate_number_placeholder');
     }
   },
- mounted() {
+  mounted() {
     const token = JSON.parse(localStorage.getItem('token'));
     if (token && isTokenExpired(token)) {
       alert("Your token is expired, please sign in again");
@@ -78,44 +76,43 @@ export default {
 </script>
 
 <template>
-   <div class="mt-8 flex   flex-column  gap-8" aria-label="Search report options content">
-    <div  class="w-full flex  flex-column  pt-8  gap-7" aria-label="Search report options parameters">
-        <div class="w-10rem md:w-15rem lg:w-20rem xl:w-30rem flex flex-column lg:mx-auto">
-          <h2 class=" text-white text-md md:text-base lg:text-xl xl:text-2xl"> {{$t('search_context')}} </h2>
-          <h2 class=" text-white text-md md:text-base lg:text-xl xl:text-2xl"> {{$t('search_organization_context')}}</h2>
+  <div class="mt-8 flex flex-column gap-8" aria-label="Search report options content">
+    <div class="w-full flex flex-column pt-8 gap-7" aria-label="Search report options parameters">
+      <div class="w-10rem md:w-15rem lg:w-20rem xl:w-30rem flex flex-column lg:mx-auto">
+        <h2 class="text-white text-md md:text-base lg:text-xl xl:text-2xl"> {{$t('search_context')}} </h2>
+        <h2 class="text-white text-md md:text-base lg:text-xl xl:text-2xl"> {{$t('search_organization_context')}}</h2>
+      </div>
+      <div v-if="notDataFound" class="flex flex-column lg:flex-row gap-2">
+        <div class="flex font-bold lg:mx-auto text-white flex-column gap-4">
+          <p class="text-xs md:text-xl">{{$t('certified_number')}}:</p>
+          <p class="text-xs md:text-xl">{{$t('cylinder_number')}}:</p>
+          <p class="text-xs md:text-xl">{{$t('vehicle_identifier_number')}}:</p>
+          <p class="text-xs md:text-xl">{{$t('emit_date')}}:</p>
         </div>
-        <div v-if="notDataFound" class="flex flex-column lg:flex-row  gap-2">
-          <div class="flex font-bold lg:mx-auto text-white flex-column gap-4">
-            <p class="text-xs md:text-xl">{{$t('certified_number')}}:</p>
-            <p class="text-xs md:text-xl">{{$t('cylinder_number')}}:</p>
-            <p class="text-xs md:text-xl">{{$t('vehicle_identifier_number')}}:</p>
-            <p class="text-xs md:text-xl">{{$t('emit_date')}}:</p>
-          </div>
-          <div class="flex flex-column lg:mx-auto w-10rem sm:w-15rem gap-2">
-            <pv-inputext class="text-xs xl:text-base" v-model="certifiedNumber" :placeholder="certifiedNumberPlaceholder" aria-label="Enter certified number"></pv-inputext>
-            <pv-inputext class="text-xs xl:text-base" v-model="cylinderNumber" :placeholder="cylinderNumberPlaceholder" aria-label="Enter cylinder number"></pv-inputext>
-            <pv-inputext class="text-xs xl:text-base" v-model="vehicleIdentifier" :placeholder="vehicleIdentifierPlaceholder" aria-label="Enter vehicle identifier"></pv-inputext>
-            <pv-inputext class="text-xs xl:text-base" v-model="emitDate" :placeholder="emitDatePlaceholder" aria-label="Enter emit date"></pv-inputext>
-            <div class="flex gap-3 flex ">
-              <pv-button @click="searchReport" aria-label="Search report pdf button">{{$t('search_button')}}</pv-button>
-              <pv-button severity="warn" @click="clearParameters" aria-label="Clear report parameters button">{{$t('clear_button')}}</pv-button>
-            </div>
+        <div class="flex flex-column lg:mx-auto w-10rem sm:w-15rem gap-2">
+          <pv-inputext class="text-xs xl:text-base" v-model="certifiedNumber" :placeholder="certifiedNumberPlaceholder" aria-label="Enter certified number"></pv-inputext>
+          <pv-inputext class="text-xs xl:text-base" v-model="cylinderNumber" :placeholder="cylinderNumberPlaceholder" aria-label="Enter cylinder number"></pv-inputext>
+          <pv-inputext class="text-xs xl:text-base" v-model="vehicleIdentifier" :placeholder="vehicleIdentifierPlaceholder" aria-label="Enter vehicle identifier"></pv-inputext>
+          <pv-inputext class="text-xs xl:text-base" v-model="emitDate" :placeholder="emitDatePlaceholder" aria-label="Enter emit date"></pv-inputext>
+          <div class="flex gap-3">
+            <pv-button @click="searchReport" aria-label="Search report pdf button">{{$t('search_button')}}</pv-button>
+            <pv-button severity="warn" @click="clearParameters" aria-label="Clear report parameters button">{{$t('clear_button')}}</pv-button>
           </div>
         </div>
+      </div>
       <div v-else class="flex flex-column gap-3" aria-label="Report content section">
-        <div class="flex gap-3 flex  ">
+        <div class="flex gap-3">
           <pv-button @click="searchReport" aria-label="Search report pdf button">{{$t('search_button')}}</pv-button>
           <pv-button severity="warn" @click="clearParameters" aria-label="Clear report parameters button">{{$t('clear_button')}}</pv-button>
         </div>
         <iframe :src="pdfImgUrl" class="pdf-screen-manager" width="900" height="1030">
-         {{$t('warning')}} <code>iframe</code>.
+          {{$t('warning')}} <code>iframe</code>.
         </iframe>
       </div>
     </div>
-     <div class="my-8 md:my-0 w-10rem md:w-15rem lg:w-20rem xl:w-30rem text-white p-2 text-xs border-round-lg text-justify sm:text-sm  lg:text-base sm:w-15rem md:w-25rem flex flex-column gap-2  lg:w-30rem lg:mx-auto" aria-label="Sign up options title">
-       <p>
-         {{$t('sign_up_information')}}</p>
-     </div>
+    <div class="my-8 md:my-0 w-10rem md:w-15rem lg:w-20rem xl:w-30rem text-white p-2 text-xs border-round-lg text-justify sm:text-sm lg:text-base sm:w-15rem md:w-25rem flex flex-column gap-2 lg:w-30rem lg:mx-auto" aria-label="Sign up options title">
+      <p>{{$t('sign_up_information')}}</p>
+    </div>
   </div>
 </template>
 
